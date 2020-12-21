72e91ff0680a3eb657bdf95aec3a9e9f733de5fb
Akndju <davidd.akindoju@gmail.com>
2020-12-21 15:14:05 +0100

    Converted entire app from redux to context API

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
-import DirectoryContext from '../../contexts/directory/directory.context';
 import MenuItem from '../menu-item/menu-item.component';
+import DirectoryContext from '../../contexts/directory/directory.context';
 import './directory.styles.scss';
 
 const Directory = () => {
-  const sections = useContext(DirectoryContext);
+  const { sections } = useContext(DirectoryContext);
+
   return (
     <div className="directory-menu">
       {sections.map(({ id, ...otherSectionProps }) => (
